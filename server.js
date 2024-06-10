0493048639ae08183ab3d624934abe6cab727470
Gabriel Carvalho <zerowings_@hotmail.com>
2024-06-09 21:42:30 -0300

    fixed

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const app = require("./app.js");
+// const http = require('http');
+// const app = require("./app.js");
 
-const port = 3000;
+// const port = 3000;
 
-const server = http.createServer(app);
-server.listen(port, () => {
-    console.log(`Server rodando na porta ${port}`);
-})
+// const server = http.createServer(app);
+// server.listen(port, () => {
+//     console.log(`Server rodando na porta ${port}`);
+// })
